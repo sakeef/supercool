906f9cd21509c1178759ba4a5dca45e551d8cb40
Muhammad Sakeeful Islam <Sakeef@Muhammads-MacBook-Pro.local>
2015-12-25 21:08:51 +0600

    Initial Commit

---
+++ Super Cool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Super Cool
+//
+//  Created by Muhammad Sakeeful Islam on 12/25/15.
+//  Copyright (c) 2015 Thinkstudio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
